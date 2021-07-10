8a6227e1c0f2f3de9620983e0dad1157b971c316
Tefo Lazuruos Keabetswe <61950677+Astro-4@users.noreply.github.com>
2021-07-10 19:07:13 +0000

    Buttons working
    Crud operations complete

--- HarrisContactManagerJava/src/ContactManager.java
+++ HarrisContactManagerJava/src/ContactManager.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.awt.EventQueue;
 import java.awt.Frame;
 
@@ -67,3 +68,74 @@ public class ContactManager {
 		frame.getContentPane().add(btnNewButton_1);
 	}
 }
+=======
+import java.awt.EventQueue;
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ContactManager {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ContactManager window = new ContactManager();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ContactManager() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 805, 455);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Personal Contacts");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				PersonalCont pc = new PersonalCont();
+				frame.dispose();
+				pc.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(188, 149, 150, 65);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Business Contacts");
+		btnNewButton_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				BusinessCont bc = new BusinessCont();
+				frame.dispose();
+				bc.setVisible(true);
+			}
+		});
+		btnNewButton_1.setBounds(468, 149, 139, 65);
+		frame.getContentPane().add(btnNewButton_1);
+	}
+}
+>>>>>>> 046a4eb104d823a7e97fba35a1ac64c33a43469a
--- HarrisContactManagerJava/src/dbConn.java
+++ HarrisContactManagerJava/src/dbConn.java
@@ -1,3 +1,170 @@
+<<<<<<< HEAD
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+public class dbConn {
+	
+	String connString = "jdbc:mysql://harrisdb.ceadcjzmmv4p.us-east-1.rds.amazonaws.com/HarrisContactDB";
+	String username = "admin";
+	String password = "509password";
+	Connection con = null;
+	
+	public dbConn() {
+		try {
+			con = DriverManager.getConnection(connString,username,password);
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Connection Failed");
+		}
+	}
+	
+	public ResultSet GetAllPersonal() {
+		
+		ResultSet rs = null;
+		String sql = "{call selectAllPersonal()}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+		
+	}
+	
+	public ResultSet GetAllBusiness() {
+			
+			ResultSet rs = null;
+			String sql = "{call selectAllBusiness()}";
+			try {
+				CallableStatement cst = con.prepareCall(sql);
+				rs = cst.executeQuery();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return rs;
+		
+	}
+	
+	public void UpdatePersonal(String f,String l,String dp,String t,String em,String a,String c,String p, String id) {
+		
+		ResultSet rs = null;
+		String sql = "{call updatePersonal(?,?,?,?,?,?,?,?,?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,id);
+			cst.setString(2,f);
+			cst.setString(3,l);
+			cst.setString(6,dp);
+			cst.setString(5,t);
+			cst.setString(4,em);
+			cst.setString(7,a);
+			cst.setString(8,c);
+			cst.setString(9,p);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void UpdateBusiness(String f,String l,String cn,String pos,String t,String em,String a,String c,String p, String id) {
+		
+		ResultSet rs = null;
+		String sql = "{call updateBusiness(?,?,?,?,?,?,?,?,?,?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,id);
+			cst.setString(2,f);
+			cst.setString(3,l);
+			cst.setString(4,cn);
+			cst.setString(5,pos);
+			cst.setString(6,t);
+			cst.setString(7,em);
+			cst.setString(8,a);
+			cst.setString(9,c);
+			cst.setString(10,p);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void InsertPersonal(String f,String l,String dp,String t,String em,String a,String c,String p) {
+		
+		ResultSet rs = null;
+		String sql = "{call insertPersonal(?,?,?,?,?,?,?,?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,f);
+			cst.setString(2,l);
+			cst.setString(5,dp);
+			cst.setString(4,t);
+			cst.setString(3,em);
+			cst.setString(6,a);
+			cst.setString(7,c);
+			cst.setString(8,p);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void InsertBusiness(String f,String l,String cn,String pos,String t,String em,String a,String c,String p) {
+		
+		ResultSet rs = null;
+		String sql = "{call insertBusiness(?,?,?,?,?,?,?,?,?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,f);
+			cst.setString(2,l);
+			cst.setString(3,cn);
+			cst.setString(4,pos);
+			cst.setString(5,t);
+			cst.setString(6,em);
+			cst.setString(7,a);
+			cst.setString(8,c);
+			cst.setString(9,p);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void DeletePersonal(String id) {
+		
+		ResultSet rs = null;
+		String sql = "{call deletePersonal(?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,id);
+			
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
+	}
+	
+	public void DeleteBusiness(String id) {
+		
+		ResultSet rs = null;
+		String sql = "{call deleteBusiness(?)}";
+		try {
+			CallableStatement cst = con.prepareCall(sql);
+			cst.setString(1,id);
+			rs = cst.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
+=======
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -96,3 +263,4 @@ public class dbConn {
 	}
 	
 }
+>>>>>>> 046a4eb104d823a7e97fba35a1ac64c33a43469a
